f4232f27d1b641879c644cb9ffaaf53d02247c50
DeLynn Berry <delynn@gmail.com>
2018-02-17 01:24:43 -0700

    Update paths.

--- paths/path.zsh
+++ paths/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$HOME/bin:$HOME/sbin:/usr/local/sbin/Applications/Postgres.app/Contents/Versions/latest/bin:$HOME/.fastlane/bin:$PATH"
+export PATH="$HOME/bin:$HOME/sbin:$PATH:/usr/local/sbin/Applications/Postgres.app/Contents/Versions/latest/bin:$HOME/.fastlane/bin"
