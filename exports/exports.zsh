d824632bb89826a7754e14076c995303b668c7b4
DeLynn Berry <delynn@gmail.com>
2019-11-26 23:22:24 -0700

    Need to freeze this as version 10 for now.

--- exports/exports.zsh
+++ exports/exports.zsh
@@ -3,4 +3,4 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Path Customization
-export PATH="/usr/local/sbin:/Applications/Postgres.app/Contents/Versions/latest/bin:$HOME/.fastlane/bin:$PATH"
+export PATH="/usr/local/sbin:/Applications/Postgres.app/Contents/Versions/10/bin:$HOME/.fastlane/bin:$PATH"
