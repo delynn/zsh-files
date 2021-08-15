0fb260c9ad32fa18c0f5c7f9b6b6d32aba3b1d5a
DeLynn Berry <delynn@gmail.com>
2021-08-15 15:10:57 -0600

    Use the standard plugin filename.

--- delynn.plugin.zsh
+++ delynn.plugin.zsh
@@ -1,1 +1,12 @@
-source ${0:A:h}/delynn.zsh
+# Aliases
+source ${0:A:h}/aliases/aliases.zsh
+
+# Functions
+source ${0:A:h}/functions/functions.zsh
+
+# Libs
+source ${0:A:h}/libs/grep.zsh
+source ${0:A:h}/libs/history.zsh
+
+# Exports
+source ${0:A:h}/exports/exports.zsh
