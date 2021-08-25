72725ce08d6856b6c42f96669e84f7543b8b105b
DeLynn Berry <delynn@gmail.com>
2021-08-24 21:34:23 -0600

    Add export for openssl.

--- exports/exports.zsh
+++ exports/exports.zsh
@@ -5,5 +5,8 @@ export LANG=en_US.UTF-8
 # NVM
 export NVM_DIR=~/.nvm
 
+# Ruby OpenSSL
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
+
 # Path Customization
 export PATH="/usr/local/sbin:/Applications/Postgres.app/Contents/Versions/latest/bin:$HOME/.fastlane/bin:$PATH"
